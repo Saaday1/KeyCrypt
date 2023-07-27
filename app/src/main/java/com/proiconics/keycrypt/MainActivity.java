443ad643d37b00b5ab8537d5ccd975c8cf9b6e2c
saaday1 <muhammadsaadu9@gmail.com>
2023-07-28 03:31:04 +0500

    Before Google Sign in

--- app/src/main/java/com/proiconics/keycrypt/MainActivity.java
+++ app/src/main/java/com/proiconics/keycrypt/MainActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-import com.proiconics.keycrypt.R;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button getStartedButton, loginTextView;
+    private ImageView btnGoogle;
+    private RelativeLayout loginButtons;
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
         getStartedButton = findViewById(R.id.getStartedButton);
         loginTextView = findViewById(R.id.loginTextView);
+        loginButtons = findViewById(R.id.loginButtons);
+        btnGoogle = loginButtons.findViewById(R.id.btnGoogle);
 
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +65,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        // Initialize GoogleSignInOptions
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build the GoogleSignInClient with the options above.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+                Log.d("MainActivity","onActivityResult");
+            }
+        });
+
     }
+
+    // Handle the Google Sign-In result in onActivityResult
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("MainActivity","onActivityResult, requestcode :"+requestCode);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleGoogleSignInResult(task);
+        }
+    }
+
+    private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, now you can use the account information
+            // You can extract user information like display name, email, profile photo, etc.
+            startActivity(new Intent(MainActivity.this,MainScreen.class));
+        } catch (ApiException e) {
+            // Sign-in failed
+            // Handle the error, e.g., display a toast or dialog
+        }
+    }
+
 }
